de7423b7c702d4e88e158a6036db060017341902
Liries1231 <liriescsgo@gmail.com>
2023-05-27 16:28:49 +0300

    ff

---
+++ src/main/java/com/example/BookProject/controller/BookController.java
@@ -0,0 +1,18 @@
+package com.example.BookProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BookController {
+
+
+    @GetMapping("/books")
+    public String book(@RequestParam (name = "name", required = false, defaultValue = "Book name") String name,
+                       Model model){
+        model.addAttribute("name",name);
+        return "books";
+    }
+}
